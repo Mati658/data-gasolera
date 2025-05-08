851f9170f3c4873257c9580156b30a75ec06221d
Mati658 <matiasaltamira@outlook.com>
2025-05-07 23:23:11 -0300

    Subida de environment -Para probar funcionamiento en prod-

---
+++ env/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  YOUTUBE_API: "AIzaSyC_2Vz1qhdk60H_h_9cAPYo1PFG3vah_kE",
+  GOOGLE_ID:
+    "659629860250-dmheagbpcjg3n89slbhoujnqnpupg6c2.apps.googleusercontent.com",
+  SUPABASE_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6cXV5eXJhYXJscGVra2prb3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNDY4MzEsImV4cCI6MjA2MDkyMjgzMX0.NrytNssJks3osJ-cK_tT-7QNJCwSU9QnPH9BjhKjqjo",
+  SUPABASE_URL: "https://fzquyyraarlpekkjkouf.supabase.co",
+};
