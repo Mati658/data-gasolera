dcdd733f9724891f345fa355c1a4a83c167176fa
Mati658 <matiasaltamira@outlook.com>
2025-05-16 15:46:17 -0300

    delete env

--- env/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  VITE_YOUTUBE_API: "AIzaSyC_2Vz1qhdk60H_h_9cAPYo1PFG3vah_kE",
-  VITE_GOOGLE_ID:
-    "659629860250-dmheagbpcjg3n89slbhoujnqnpupg6c2.apps.googleusercontent.com",
-  VITE_SUPABASE_KEY:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6cXV5eXJhYXJscGVra2prb3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNDY4MzEsImV4cCI6MjA2MDkyMjgzMX0.NrytNssJks3osJ-cK_tT-7QNJCwSU9QnPH9BjhKjqjo",
-  VITE_SUPABASE_URL: "https://fzquyyraarlpekkjkouf.supabase.co",
-  VITE_TINY_API_KEY: "jnpi1h9m5us7zjeww0o7qdeaukwq5d4s7me1r51yvmzpkovd",
-};
