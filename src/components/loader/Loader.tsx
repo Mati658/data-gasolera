3e8a0bb19a782d649d256c6e2988fa477e5b978c
Mati658 <matiasaltamira@outlook.com>
2025-05-23 02:24:22 -0300

    Cambio del logo del Loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -10,7 +10,7 @@ export default function Loader() {
     return (
       <div className={flagLoader ? 'loader-home background-loader' : 'hidden'}>
           <div className='conatiner-loader'>
-              <img src="/icon.png" className="img-loader" />
+              <img src="/loader.png" className="img-loader" />
               <p className='pulse-loader'></p>
           </div>
       </div>
@@ -19,7 +19,7 @@ export default function Loader() {
   return (
     <div className={flagLoader ? 'loader' : 'hidden'}>
         <div className='conatiner-loader'>
-            <img src="/icon.png" className="img-loader" />
+            <img src="/loader.png" className="img-loader" />
             <p className='pulse-loader'></p>
         </div>
     </div>
